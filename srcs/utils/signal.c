0f897820d58418e305d037639f817bfeb59bd0b4
Anysia <anysia74@hotmail.com>
2021-08-17 14:01:32 +0200

    Change the name of sigint handler

--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -6,7 +6,7 @@
 /*   By: cmorel-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 11:33:33 by cmorel-a          #+#    #+#             */
-/*   Updated: 2021/06/15 13:38:48 by cmorel-a         ###   ########.fr       */
+/*   Updated: 2021/08/17 14:01:06 by cmorel-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Ctrl + \ :SIGQUIT (ignore) / Ctrl + c : SIGINT (handle)
 Ctrl + D : send EOF
 */
 
-static void	handle_fatal_error(int signal)
+static void	handle_sigint(int signal)
 {
 	if (signal == SIGINT)
 	{
@@ -34,7 +34,7 @@ static void	handle_signal_error(t_minishell *minishell)
 
 void	register_signal(t_minishell *minishell)
 {
-	if (signal(SIGINT, handle_fatal_error) == SIG_ERR)
+	if (signal(SIGINT, handle_sigint) == SIG_ERR)
 		handle_signal_error(minishell);
 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
 		handle_signal_error(minishell);
